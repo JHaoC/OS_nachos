a2bffab51df130b0820c0c68a23937ae5eabbb95
Junhao <juchen@syr.edu>
2019-09-17 13:59:26 -0400

    origin Lab3

--- code/threads/threadtest.cc
+++ code/threads/threadtest.cc
@@ -1,12 +1,11 @@
 #include "kernel.h"
 #include "main.h"
 #include "thread.h"
-#include "../userprog/lab2.h"
 
 void
 ThreadTest()
 {
-    printf("Lab2 Supermarket\n");
-    Supermarket sm = Supermarket();
-    sm.Run();
+    printf("Lab3 Supermarket\n");
+    //Supermarket sm = Supermarket();
+    //sm.Run();
 }
